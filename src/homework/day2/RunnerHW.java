8457285a046c7ec61e3aaa26742533619290a8e2
Anna Sinchenko <annasinchenko31@gmail.com>
2025-09-26 13:09:30 +0300

    Playground Generic Task

---
+++ src/homework/day2/RunnerHW.java
@@ -0,0 +1,10 @@
+package homework.day2;
+
+import static homework.day2.Picture.Cow;
+
+public class RunnerHW {
+    public static void main(String[] args) {
+        System.out.println("Cow");
+        Cow();
+    }
+}
